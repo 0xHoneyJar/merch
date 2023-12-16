d49e43936c813422bddd3c83c21088be923b8182
exp_table <76456212+exp-table@users.noreply.github.com>
2023-12-16 12:44:20 +0100

    feat: add discounts (#2)
    * Moved to Item struct
    
    * forge install: solady
    
    v0.0.148
    
    * switched partially to solady
    
    * adapted tests to new struct
    
    * added test
    
    * some values constants now
    
    * fix bug in mint
    
    * discount testing

--- src/Drip.sol
+++ src/Drip.sol
@@ -1,135 +1,77 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC1155} from "solmate/tokens/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC1155 } from "solmate/tokens/ERC1155.sol";
+import { Ownable } from "solady/src/auth/Ownable.sol";
+import { LibString } from "solady/src/utils/LibString.sol";
+import { SafeTransferLib } from "solady/src/utils/SafeTransferLib.sol";
+
+interface ERC721 {
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
 
 contract Drip is ERC1155, Ownable {
-    using Strings for uint256;
-
-    mapping(uint256 => uint256) public idToPrice;
-    mapping(uint256 => uint256) public idToMaxSupply;
-    mapping(uint256 => uint256) public idToCurrentSupply;
-    mapping(uint256 => uint256) public idToOpeningTime;
-
-    uint256 spacing = 1 days;
-
-    string public baseURI = "https://honey-interface-git-claim-0xhoneyjar-s-team.vercel.app/api/metadata_merch/";
-
     error InvalidID();
     error MintNotOpen();
     error InsufficientFunds();
     error ExceedsMaxSupply();
 
-    constructor() ERC1155() Ownable(msg.sender) {
-        // Bucket hats 0.08
-        idToPrice[73] = 0.08 ether;
-        idToPrice[74] = 0.08 ether;
-        idToPrice[75] = 0.08 ether;
-
-        idToMaxSupply[73] = 23;
-        idToMaxSupply[74] = 23;
-        idToMaxSupply[75] = 23;
-
-        // Fishing hats 0.09
-        idToPrice[76] = 0.09 ether;
-        idToPrice[77] = 0.09 ether;
-        idToPrice[78] = 0.09 ether;
-
-        idToMaxSupply[76] = 23;
-        idToMaxSupply[77] = 23;
-        idToMaxSupply[78] = 23;
-
-        // Shirts 0.1
-        idToPrice[79] = 0.1 ether;
-        idToPrice[80] = 0.1 ether;
-        idToPrice[81] = 0.1 ether;
-        idToPrice[82] = 0.1 ether;
-        idToPrice[83] = 0.1 ether;
-        idToPrice[84] = 0.1 ether;
-        idToPrice[85] = 0.1 ether;
-        idToPrice[86] = 0.1 ether;
-        idToPrice[87] = 0.1 ether;
-        idToPrice[88] = 0.1 ether;
-        idToPrice[89] = 0.1 ether;
-        idToPrice[90] = 0.1 ether;
-
-        idToMaxSupply[79] = 35;
-        idToMaxSupply[80] = 35;
-        idToMaxSupply[81] = 35;
-        idToMaxSupply[82] = 35;
-        idToMaxSupply[83] = 35;
-        idToMaxSupply[84] = 35;
-        idToMaxSupply[85] = 35;
-        idToMaxSupply[86] = 35;
-        idToMaxSupply[87] = 35;
-        idToMaxSupply[88] = 35;
-        idToMaxSupply[89] = 35;
-        idToMaxSupply[90] = 35;
-
-        // Hoodies 0.2
-        idToPrice[91] = 0.2 ether;
-        idToPrice[92] = 0.2 ether;
-        idToPrice[93] = 0.2 ether;
-        idToPrice[94] = 0.2 ether;
-
-        idToMaxSupply[91] = 17;
-        idToMaxSupply[92] = 17;
-        idToMaxSupply[93] = 18;
-        idToMaxSupply[94] = 17;
-
-        uint256 initialDropTime = 1672444800; // Example: January 1, 2023
-
-        // Bucket hats 0.08
-        idToOpeningTime[73] = initialDropTime;
-        idToOpeningTime[74] = initialDropTime;
-        idToOpeningTime[75] = initialDropTime;
-
-        // Fishing hats 0.09 (after Bucket hats)
-        initialDropTime += spacing;
-        idToOpeningTime[76] = initialDropTime;
-        idToOpeningTime[77] = initialDropTime;
-        idToOpeningTime[78] = initialDropTime;
-
-        // Shirts 0.1 (after Fishing hats)
-        initialDropTime += spacing;
-        idToOpeningTime[79] = initialDropTime;
-        idToOpeningTime[80] = initialDropTime;
-        idToOpeningTime[81] = initialDropTime;
-        idToOpeningTime[82] = initialDropTime;
-        idToOpeningTime[83] = initialDropTime;
-        idToOpeningTime[84] = initialDropTime;
-        idToOpeningTime[85] = initialDropTime;
-        idToOpeningTime[86] = initialDropTime;
-        idToOpeningTime[87] = initialDropTime;
-        idToOpeningTime[88] = initialDropTime;
-        idToOpeningTime[89] = initialDropTime;
-        idToOpeningTime[90] = initialDropTime;
-
-        // Hoodies 0.2 (after Shirts)
-        initialDropTime += spacing;
-        idToOpeningTime[91] = initialDropTime;
-        idToOpeningTime[92] = initialDropTime;
-        idToOpeningTime[93] = initialDropTime;
-        idToOpeningTime[94] = initialDropTime;
+    struct Item {
+        uint128 price;
+        uint32 maxSupply;
+        uint32 currentSupply;
+        uint64 openingTime;
     }
 
-    function uri(uint256 id) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, id.toString()));
+    mapping(uint256 => Item) public idToItem;
+    uint256 constant MAX_DISCOUNT = 20;
+    ERC721 constant honeycombs = ERC721(0xCB0477d1Af5b8b05795D89D59F4667b59eAE9244);
+    string constant public baseURI = "https://honey-interface-git-claim-0xhoneyjar-s-team.vercel.app/api/metadata_merch/";
+
+    constructor() ERC1155() {
+        _initializeOwner(msg.sender);
     }
 
-    function mint(uint256 id, uint256 quantity) public payable {
-        if (idToPrice[id] <= 0) revert InvalidID();
-        if (block.timestamp < idToOpeningTime[id]) revert MintNotOpen();
-        if (msg.value != idToPrice[id] * quantity) revert InsufficientFunds();
-        if (idToCurrentSupply[id] + quantity > idToMaxSupply[id]) revert ExceedsMaxSupply();
+    function uri(uint256 id) public pure override returns (string memory) {
+        return LibString.concat(baseURI, LibString.toString(id));
+    }
 
-        idToCurrentSupply[id] += quantity;
-        _mint(msg.sender, id, quantity, "");
+    function mint(uint256 id, uint32 quantity) public payable {
+        Item memory item = idToItem[id];
+        if (item.price == 0) revert InvalidID();
+        if (block.timestamp < item.openingTime) revert MintNotOpen();
+        if (msg.value != item.price * quantity) revert InsufficientFunds();
+        if (item.currentSupply + quantity > item.maxSupply) revert ExceedsMaxSupply();
+
+        uint256 honeycombsBalance = honeycombs.balanceOf(msg.sender);
+        // cap discount at 20%
+        // 1 honeycomb = 1% discount
+        uint256 discount = honeycombsBalance > MAX_DISCOUNT ? MAX_DISCOUNT : honeycombsBalance;
+        uint256 toRefund = honeycombsBalance > 0 ? (item.price * quantity * discount) / 100 : 0;
+
+        idToItem[id].currentSupply += quantity;
+        _mint(msg.sender, id, uint256(quantity), "");
+        SafeTransferLib.safeTransferETH(msg.sender, toRefund);
     }
 
     function withdraw() public onlyOwner {
-        payable(msg.sender).transfer(address(this).balance);
+        SafeTransferLib.safeTransferAllETH(msg.sender);
+    }
+
+    function setItems(
+        uint256 dropTime,
+        uint256[] calldata ids,
+        uint256[] calldata prices,
+        uint256[] calldata maxSupply
+    ) public onlyOwner {
+        uint256 length = ids.length;
+        for (uint256 i; i < length; i++) {
+            idToItem[ids[i]] = Item({
+                price: uint128(prices[i]),
+                maxSupply: uint32(maxSupply[i]),
+                currentSupply: 0,
+                openingTime: uint64(dropTime)
+            });
+        }
     }
 }
--- test/Drip.t.sol
+++ test/Drip.t.sol
@@ -2,18 +2,52 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console2.sol";
 import {Drip} from "../src/Drip.sol";
 import {ERC1155TokenReceiver} from "solmate/tokens/ERC1155.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {Solarray} from "solarray/Solarray.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+contract HoneyCombs is ERC721 {
+    uint256 counter;
+    constructor() ERC721("honeycombs", "hc") {}
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "";
+    }
+    function mint(address to, uint256 amount) public {
+        for (uint256 i = 0; i < amount; i++) {
+            _mint(to, counter++);
+        }
+    }
+}
 
 contract DripTest is Test, ERC1155TokenReceiver {
     Drip public drip;
 
+    uint256 currentTime = 1672444800;
+    HoneyCombs honeycombs;
+
     function setUp() public {
+        // etch honeycomb contract on mainnet address
+        honeycombs = new HoneyCombs();
+        vm.etch(address(0xCB0477d1Af5b8b05795D89D59F4667b59eAE9244), address(honeycombs).code);
+        honeycombs = HoneyCombs(address(0xCB0477d1Af5b8b05795D89D59F4667b59eAE9244));
+
         drip = new Drip();
+        
+        vm.warp(currentTime + 2 days);
+        uint256 dropTime = currentTime;
+
+        uint256[] memory ids = Solarray.uint256s(73, 76);
+        uint256[] memory prices = Solarray.uint256s(0.08 ether, 0.09 ether);
+        uint256[] memory maxSupplies = Solarray.uint256s(23, 1);
+        drip.setItems(dropTime, ids, prices, maxSupplies);
+
     }
 
     function testMint() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         drip.mint{value: 0.08 ether}(73, 1);
         assertEq(drip.balanceOf(address(this), 73), 1);
@@ -23,42 +57,42 @@ contract DripTest is Test, ERC1155TokenReceiver {
     }
 
     function testMintOutOfBoundsAboveRevert() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         vm.expectRevert(Drip.InvalidID.selector);
         drip.mint{value: 0.1 ether}(100, 1);
     }
 
     function testMintOutOfBoundsBelowRevert() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         vm.expectRevert(Drip.InvalidID.selector);
         drip.mint{value: 0.1 ether}(1, 1);
     }
 
     function testMintInsufficientFundsRevert() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         vm.expectRevert(Drip.InsufficientFunds.selector);
         drip.mint{value: 0.08 ether}(76, 1);
     }
 
     function testOverpayRevert() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         vm.expectRevert(Drip.InsufficientFunds.selector);
         drip.mint{value: 10 ether}(73, 2);
     }
 
     function testExceedsSupplyRevert() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         vm.expectRevert(Drip.ExceedsMaxSupply.selector);
         drip.mint{value: 1.92 ether}(73, 24);
     }
 
     function testWithdrawFunds() public {
-        vm.warp(1672444800 + 2 days);
+        vm.warp(currentTime + 2 days);
 
         drip.mint{value: 0.08 ether}(73, 1);
         assertEq(drip.balanceOf(address(this), 73), 1);
@@ -69,6 +103,33 @@ contract DripTest is Test, ERC1155TokenReceiver {
         uint256 balance = address(this).balance;
         drip.withdraw();
         assertEq(address(this).balance, balance + 0.17 ether);
+    }
+
+    function testAttackerCannotWithdrawFunds() public {
+        vm.warp(currentTime + 2 days);
+
+        drip.mint{value: 0.08 ether}(73, 1);
+        drip.mint{value: 0.09 ether}(76, 1);
+
+        vm.prank(address(0xfeef));
+        vm.expectRevert();
+        drip.withdraw();
+    }
+
+    function testDiscount(uint256 combsToUse, uint256 amountToBuy) public {
+        // 1 comb = 1% discount
+        uint256 combsToUse = StdUtils.bound(combsToUse, 0, 20);
+        uint256 amountToBuy = StdUtils.bound(amountToBuy, 0, 23);
+
+        // mint hcs for discount
+        honeycombs.mint(address(this), combsToUse);
+        uint256 balanceBefore = address(this).balance;
+        uint256 toPay = 0.08 ether * amountToBuy;
+        // mint drip
+        drip.mint{value: toPay}(73, uint32(amountToBuy));
+        // balance should have diminished by (0.08 * amount) / (100 - combsToUse)
+        uint256 expectedRefund = (toPay * combsToUse) / 100;
+        assertEq(address(this).balance, balanceBefore - toPay + expectedRefund);
     }
 
     receive() external payable {}
